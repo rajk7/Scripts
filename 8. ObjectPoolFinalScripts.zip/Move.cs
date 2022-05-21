aec10d51f5ed64ed35f49d9c21e3f1c49ec6a73f
rajk7 <50164635+rajk7@users.noreply.github.com>
2022-05-21 07:00:37 +0530

    Add files via upload

---
+++ 8. ObjectPoolFinalScripts.zip/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 velocity;
+    public AudioSource exp;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "asteroid")
+        {
+            collision.gameObject.SetActive(false);
+            exp.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.Translate(velocity);
+	}
+}
